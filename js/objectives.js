22165caa14ca2e4d58828c8560a67c2317940412
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-10 12:01:52 +0300

    create objects for questions and options. Then store in array.

--- js/objectives.js
+++ js/objectives.js
@@ -1,11 +1,36 @@
-//Store questions and answers in an array
-//var py: for Python and var ruby: for Ruby --- for future improvements.
+/*Pseudocode
+
+1. Link JS file in index.html.
+Test to check JS file is linked correctly
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+});
+
+2. store Q and A in Array.
+var jsTrivia = [];
+
+3. call on function to display questions and options
+var displayTrivia = function();
+
+3. Gather input from user and store in an Array.
+var userInput = [];
+
+3. Check user input against answer sheet.
+
+3. Display Score.
+
+
+------------------------------------------------------------------------------------------*/
+// STEP 2: store Questions and Options in an Array.
+//var pyTrivia: for Python and var triviaRuby: for Ruby --- for future extended features.
 
 var jsTrivia = [
 // Q - 1
 {
-  trivia: "Which of the following is correct about features of JavaScript?",
-  answers: {
+  question: "Which of the following is correct about features of JavaScript?",
+  options: {
     A: "JavaScript is a lightweight, interpreted programming language.",
     B: "JavaScript is designed for creating network-centric applications.",
     C: "JavaScript is complementary to and integrated with Java.",
@@ -16,8 +41,8 @@ var jsTrivia = [
 
 // Q - 2
 {
-  trivia: "Which of the following is the correct syntax to print a page using JavaScript?",
-  answers: {
+  question: "Which of the following is the correct syntax to print a page using JavaScript?",
+  options: {
     A: "window.print();",
     B: "browser.print();",
     C: "navigator.print();",
@@ -28,8 +53,8 @@ var jsTrivia = [
 
 // Q - 3
 {
-  trivia: "Which built-in method calls a function for each element in the array?",
-  answers: {
+  question: "Which built-in method calls a function for each element in the array?",
+  options: {
     A: "while()",
     B: "loop()",
     C: "forEach()",
@@ -40,10 +65,10 @@ var jsTrivia = [
 
 // Q - 4
 {
-  trivia: "Which built-in method calls a function for each element in the array?",
-  answers: {
+  question: "Which built-in method calls a function for each element in the array?",
+  options: {
     A: "toUpperCase()",
-    B: "toUpper(),
+    B: "toUpper()",
     C: "changeCase(case)",
     D: "None of the above."
   },
@@ -52,8 +77,8 @@ var jsTrivia = [
 
 // Q - 5
 {
-  trivia: " Which of the following function of String object combines the text of two strings and returns a new string?",
-  answers: {
+  question: " Which of the following function of String object combines the text of two strings and returns a new string?",
+  options: {
     A: "add()",
     B: "merge()",
     C: "concat()",
@@ -64,8 +89,8 @@ var jsTrivia = [
 
 // Q - 6
 {
-  trivia: "Which of the following function of String object splits a String object into an array of strings by separating the string into substrings?",
-  answers: {
+  question: "Which of the following function of String object splits a String object into an array of strings by separating the string into substrings?",
+  options: {
     A: "slice()",
     B: "merge()",
     C: "replace()",
@@ -76,10 +101,10 @@ var jsTrivia = [
 
 // Q - 7
 {
-  trivia: "Which of the following function of String object returns the primitive value of the specified object.",
-  answers: {
+  question: "Which of the following function of String object returns the primitive value of the specified object.",
+  options: {
     A: "toLocaleUpperCase()",
-    B: "toUpperCase(),
+    B: "toUpperCase()",
     C: "toString()",
     D: "valueOf()"
   },
@@ -88,10 +113,10 @@ var jsTrivia = [
 
 // Q - 8
 {
-  trivia: "Which of the following function of String object causes a string to be displayed in fixed-pitch font as if it were in a <tt> tag?",
-  answers: {
+  question: "Which of the following function of String object causes a string to be displayed in fixed-pitch font as if it were in a <tt> tag?",
+  options: {
     A: "fixed()",
-    B: "big(),
+    B: "big()",
     C: "blink()",
     D: "bold()"
   },
@@ -100,10 +125,10 @@ var jsTrivia = [
 
 // Q - 9
 {
-  trivia: "Which of the following function of Array object creates a new array with the results of calling a provided function on every element in this array?",
-  answers: {
+  question: "Which of the following function of Array object creates a new array with the results of calling a provided function on every element in this array?",
+  options: {
     A: "push()",
-    B: "join(),
+    B: "join()",
     C: "pop()",
     D: "map()"
   },
@@ -112,8 +137,8 @@ var jsTrivia = [
 
 // Q - 10
 {
-  trivia: "Which of the following function of Array object sorts the elements of an array?",
-  answers: {
+  question: "Which of the following function of Array object sorts the elements of an array?",
+  options: {
     A: "toSource()",
     B: "sort()",
     C: "toString()",
@@ -123,4 +148,46 @@ var jsTrivia = [
 },
 ];
 
-//Store questions and answers in an array
+//variables for retrieving and storing id's: trivia, submit and results.
+var triviaContainer = document.getElementById('trivia').;
+var resultsContainer = document.getElementById('submit').;
+var submitButton = document.getElementById('results').;
+
+
+  function displayTrivia() {
+
+    var showQuestions = [];
+    //for each question.
+    jsTrivia.forEach((currentQuestion, questionNumber) => {
+    //store options in an Array.
+    var options = [];
+    //for each available answer.
+    for (var letter in currentQuestion.options) {
+      //store options in checkboxes.
+      options.push(
+
+        `<label class="radio-inline">
+          <input type="radio" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.options[letter]}
+        </label>`
+      );
+    }
+      showQuestions.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${options.join("")} </div>`
+      );
+    });
+
+    //combine list and display on html.
+    triviaContainer.innerHTML = showQuestions.join("");
+  };
+
+//call on function to display.
+
+  //End Function for displaying Trivia on html.
+
+
+  // <div id="trivia" class="hs-line-8 no-transp font-alt mb-40 mb-xs-20">
+  //   <p></p>
+  // </div>
